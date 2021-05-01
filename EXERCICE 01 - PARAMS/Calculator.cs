e6578c80412c00db7acd7c76159286e98d2d8117
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-01 10:36:59 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICE 01 - PARAMS/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCICE_01___PARAMS {
+    class Calculator {
+        //---------------------------- START --------------------------------//
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        //-------------------------------- END -------------------------------//
+    }
+}
